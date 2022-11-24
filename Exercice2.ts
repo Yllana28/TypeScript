645e7b61587b7f66a26424ce33f59e7f90198bbe
Lamia <boudedjalamia@gmail.com>
2022-11-24 15:31:40 +0100

    feat: exercice2

--- Exercice2.ts
+++ Exercice2.ts
@@ -1,4 +1,21 @@
-function add(a: number, b: number, c?: number) {
-    return a + b + (c || 0);
-}
-console.log(add(2,5));
+
+
+
+// function add(a: number, b: number, c?: number) {
+//     return a + b + (c || 0);
+// }
+// console.log(add(2,5);
+
+// ///////////////////////////////
+
+/**
+ * Réaliser une addition a partire de deux nombres
+ * @param nombre1 premier nombre
+ * @param nombre2 second nombre
+ */
+ function addition(nombre1: number, nombre2: number,): void{
+    const resultat: number = nombre1 + nombre2
+    console.log(resultat);
+    
+ }
+addition(5,5)
