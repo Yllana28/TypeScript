0bf2081106c26dfcee4f98eb61b60575b1431e89
Lamia <boudedjalamia@gmail.com>
2022-11-29 10:00:23 +0100

    feat: chat

--- Chat.ts
+++ Chat.ts
@@ -17,31 +17,84 @@
 // }
 // function surnom(...surnoms: string[])
 
-class Chat{
-    nom!: string;
-    race!: string;
-    age!: number;
-    poids!: number;
+// class Chat{
+//     nom!: string;
+//     race!: string;
+//     age!: number;
+//     poids!: number;
 
-afficherChat(): void {
+// afficherChat(): void {
 
-    console.log(`
-        Mon super chat s'appelle ${this.nom} \r 
-        C'st un ${this.race} \r 
-        Il a ${this.age} \r
-        Et il pèse ${this.poids} kg!
+//     console.log(`
+//         Mon super chat s'appelle ${this.nom} \r 
+//         C'st un ${this.race} \r 
+//         Il a ${this.age} \r
+//         Et il pèse ${this.poids} kg!
 
-    `)
+//     `)
     
+//     }
+// }
+
+// const monChat: Chat = new Chat();
+
+// monChat.age = 2;
+// monChat.nom = "Minou";
+// monChat.poids = 50;
+// monChat.race = "Calinou";
+
+// monChat.afficherChat();
+
+
+abstract class Chat{
+    protected _nom: string;
+    protected _age: number;
+    protected _poids: number;
+
+    constructor (nom: string, age: number, poids: number){
+        this._nom = nom;
+        this._age = age;
+        this._poids = poids;
+
+    }
+    public get nom(): string {
+        return this._nom;
+    }
+
+    public get age():number{
+        return this._age;
+    }
+
+    public get poids(): number{
+        return this._poids;
+    }
+
+    public afficherChat(){
+        console.log(`Voici mon chat, il s'appelle ${this.nom}, il a ${this.age} ans, son poids ${this.poids} kg ! `);
     }
 }
+    class Europeen extends Chat {
+        _surnoms: string[];
+        _origine: string = `Europe`;
 
-const monChat: Chat = new Chat();
+        constructor (nom: string, age: number, poids: number, ...surnoms: string[]){
+            super(nom, age, poids);
+            this._surnoms = surnoms;
+        }
+        get surnoms(): string [] {
+            return this._surnoms
 
-monChat.age = 2;
-monChat.nom = "Minou";
-monChat.poids = 50;
-monChat.race = "Calinou";
+        }
+        set surnoms(surnoms: string[]) {
+            surnoms.forEach(surnoms => {this._surnoms.push(surnoms)})
+        }
 
-monChat.afficherChat();
+    }
+
+   const chat: Europeen = new Europeen(`Minou`, 0.5, 5,`TrucQuiPue`);
+   chat.afficherChat();
+   chat.surnoms = ['Kiki', 'Choco'];
+   console.log(chat.surnoms);
+
+
 
